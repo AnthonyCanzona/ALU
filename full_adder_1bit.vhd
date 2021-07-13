2ed4d927c8acef3c5f86e3b5fce8fb33b9a7cedf
AnthonyCanzona <86177803+AnthonyCanzona@users.noreply.github.com>
2021-07-12 21:58:25 -0400

    Update full_adder_1bit.vhd

--- full_adder_1bit.vhd
+++ full_adder_1bit.vhd
@@ -7,9 +7,9 @@ library work;
 
 entity full_adder_1bit is
  	port (
-			cin, bit_val1, bit_val2 	: in std_logic;
-			bit_sum 							: out std_logic;
-		   carry_out_bit					: out std_logic
+		cin, bit_val1, bit_val2 	: in std_logic;
+		bit_sum 			: out std_logic;
+		carry_out_bit			: out std_logic
 			);
 end full_adder_1bit;
 
@@ -29,15 +29,15 @@ signal half_adder_sum, half_adder_carry	: std_logic;
 
 begin
 
-half_adder_carry 		<= bit_val1 AND bit_val2;
+half_adder_carry 	<= bit_val1 AND bit_val2;
 
 half_adder_sum 		<= bit_val1 XOR bit_val2;
 
 -- The Boolean equation for bit_sum
-bit_sum 					<= half_adder_sum XOR cin;
+bit_sum 		<= half_adder_sum XOR cin;
  
 -- The Boolean equation for carry_out_bit
-carry_out_bit 			<= (half_adder_sum AND cin) OR half_adder_carry ;
+carry_out_bit 		<= (half_adder_sum AND cin) OR half_adder_carry ;
 
 
 end;
