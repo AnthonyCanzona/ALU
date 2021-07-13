dec38af04365cf5f135879d052fdf08c02e3a5ed
AnthonyCanzona <86177803+AnthonyCanzona@users.noreply.github.com>
2021-07-12 21:59:23 -0400

    Update full_adder_4bit.vhd

--- full_adder_4bit.vhd
+++ full_adder_4bit.vhd
@@ -7,10 +7,10 @@ library work;
 
 entity full_adder_4bit is
  	port (
-			cin							: in std_logic;
-			hex_val_A, hex_val_B 	: in std_logic_vector(3 downto 0);
-			hex_sum 						: out std_logic_vector(3 downto 0);
-		   carry_out					: out std_logic
+		cin			: in std_logic;
+		hex_val_A, hex_val_B 	: in std_logic_vector(3 downto 0);
+		hex_sum 		: out std_logic_vector(3 downto 0);
+		carry_out		: out std_logic
 			);
 end full_adder_4bit;
 
@@ -21,9 +21,9 @@ architecture Circuit of full_adder_4bit is
  
 component full_adder_1bit
  	port (
-			cin, bit_val1, bit_val2 	: in std_logic;
-			bit_sum 							: out std_logic;
-		   carry_out_bit					: out std_logic
+		cin, bit_val1, bit_val2 	: in std_logic;
+		bit_sum 			: out std_logic;
+		carry_out_bit			: out std_logic
 			);
 end component;
  
@@ -35,7 +35,7 @@ end component;
 -------------------------------------------------------------------
 
 -- group of 4 logic signals with the group type defined as std_logic_vector(MSB downto LSB)
-	signal cout	: std_logic_vector(3 downto 0);
+signal cout	: std_logic_vector(3 downto 0);
 
 -------------------------------------------------------------------
 -------------------------------------------------------------------
@@ -46,7 +46,6 @@ begin
 adder0: full_adder_1bit port map (cin, hex_val_A(0), hex_val_B(0),hex_sum(0), cout(0));
 											
 adder1: full_adder_1bit port map (cout(0), hex_val_A(1), hex_val_B(1), hex_sum(1), cout(1));
-
 
 -- Instance connections for adder2 and adder3
 											
